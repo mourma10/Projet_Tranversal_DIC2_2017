b29e2b7b0172a3dc84c6854b6d0b19b86a2fba86
MAC <bigathepa>
2017-07-11 01:56:43 +0000

    quelques modifications et page trading

---
+++ app/dashboard/trading/trading.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'trading-cmp',
+    moduleId: module.id,
+    templateUrl: 'trading.component.html'
+})
+
+export class TradingComponent {
+}
